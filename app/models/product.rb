bcfb0a06033cb5ed707125c7a48a79928c6533ed
Moiz K <moiz@heuretix.com>
2013-03-19 21:20:17 -0400

    Create Products now works again

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ActiveRecord::Base
   belongs_to :brand
   belongs_to :area
   belongs_to :category
-  attr_accessible :description, :link, :name, :price
+  attr_accessible :description, :link, :name, :price, :brand_id, :area_id, :category_id
 end
