f905a8caa45652cfced93631195f6bbb51bfba51
David Mazza <github@dmzza.com>
2013-03-26 15:19:01 -0400

    search for postgres

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,10 +5,10 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :link, :name, :price, :brand_id, :area_id, :category_id
 
   def self.search(query)
-  	find(:all, :conditions => ['name LIKE ?', "%#{query}%"])
+  	find(:all, :conditions => ['name ilike ? or description ilike ?', "%#{query}%", "%#{query}%"])
   end
 
   def self.similar(query)
-		find(:all, :conditions => ['name NOT LIKE ?', "%#{query}%"])
+		find(:all, :conditions => ['name NOT ilike ? and description not ilike ?', "%#{query}%", "%#{query}%"])
   end
 end
